c060ed7d74e864444d72ab7efae1af8e94218090
praneeth <praneeth@praneeths-MacBook-Pro.local>
2020-04-05 16:55:35 +0530

    Git Android Studio First Commit

---
+++ app/src/main/java/com/example/gofree/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.gofree;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.parse.ParseInstallation;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+}
